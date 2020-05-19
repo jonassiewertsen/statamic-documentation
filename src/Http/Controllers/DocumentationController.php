d59d122a9fa54aa8814f2630e6b2cc1eea746ae6
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-05-19 23:08:00 +0200

    Use the bard editor instead of markdown

--- src/Http/Controllers/DocumentationController.php
+++ src/Http/Controllers/DocumentationController.php
@@ -4,10 +4,14 @@ namespace Jonassiewertsen\Documentation\Http\Controllers;
 
 use Jonassiewertsen\Documentation\Helper\Documentation;
 use Statamic\Facades\Entry;
+use Statamic\Fieldtypes\Bard;
 
 class DocumentationController {
     public function show($slug) {
         $documentation = Entry::findBySlug($slug, Documentation::collectionName());
+
+        $bard = new Bard();
+        $documentation->content = $bard->augment($documentation->content);
 
         return view('documentation::cp.show', compact('documentation'));
     }
