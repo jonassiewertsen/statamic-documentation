3384e9879916b12918c5a87ae512feb4e6eb418c
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-06-26 23:01:34 +0200

    Better documentation

--- src/Http/Controllers/DocumentationController.php
+++ src/Http/Controllers/DocumentationController.php
@@ -6,18 +6,27 @@ use Jonassiewertsen\Documentation\Helper\Documentation;
 use Statamic\Facades\Entry;
 
 class DocumentationController {
+    /**
+     * Our Controller for to show parent entries
+     */
     public function show($slug) {
         return view('documentation::cp.show',[
             'documentation' => $this->getEntry($slug),
         ]);
     }
 
+    /**
+     * Our Controller for to show child entries
+     */
     public function showChild($parent, $slug) {
         return view('documentation::cp.show',[
             'documentation' => $this->getEntry($slug),
         ]);
     }
 
+    /**
+     * Fetching an entry by slug
+     */
     private function getEntry($slug) {
         $documentation = Entry::findBySlug($slug, Documentation::collectionName());
         return $documentation->toAugmentedArray(['title', 'content']);
