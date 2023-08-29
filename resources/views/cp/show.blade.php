80e867ee5e01c6ee7778d62755fa088c50fb6cc7
Christoph Stockinger <christoph@morethings.digital>
2023-08-29 22:05:21 +0200

    fix(views): design adjustment

--- resources/views/cp/show.blade.php
+++ resources/views/cp/show.blade.php
@@ -2,12 +2,13 @@
 @inject('modify', 'Statamic\Modifiers\Modify')
 
 @extends('statamic::layout')
+@section('title', $documentation['title'])
 
 @section('content')
 
     <h1 class="mt-2 mb-3 w-full text-4xl text-grey-70">{{ $documentation['title'] }}</h1>
 
-    <section class="card px-6 pt-7 pb-6">
+    <section class="card px-16 py-8">
         <article class="bard-fieldtype">
             <div class="ProseMirror">
 
