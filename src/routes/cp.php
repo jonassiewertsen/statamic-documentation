64116bc6f3bcfa8d6b6ec32770c644f3d5159b7d
Christoph Stockinger <christoph@morethings.digital>
2023-08-29 22:04:59 +0200

    fix(routes): added namespaces

--- src/routes/cp.php
+++ src/routes/cp.php
@@ -1,5 +1,9 @@
 <?php
 
 /** Documentation */
-Route::get('/documentation/{slug}', 'DocumentationController@show')->name('documentation.show');
-Route::get('/documentation/{parent}/{slug}', 'DocumentationController@showChild')->name('documentation.child.show');
+
+use Jonassiewertsen\Documentation\Http\Controllers\DocumentationController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/documentation/{slug}', [DocumentationController::class, 'show'])->name('documentation.show');
+Route::get('/documentation/{parent}/{slug}', [DocumentationController::class, 'showChild'])->name('documentation.child.show');
